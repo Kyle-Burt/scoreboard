fb29777e25a90321cdfed86bf52685a27f78962c
Kyle <Kybt11@gmail.com>
2023-06-12 17:00:31 -0600

    JS update

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 
 let home = 0
-
 let away = 0
 
 
 console.log(`Home`, home)
 console.log(`Away`, away)
-
-let totalHomeScore = home
-let totalAwayScore = away
 
 
 let awayScore = document.getElementById(`awayScore`)
@@ -20,24 +16,22 @@ console.log(home)
 function homeButton1() {
     console.log(`Home`, home += 1)
     let homeScore = document.getElementById(`homeScore`)
-    console.log(home)
     homeScore.innerText = home
-    console.log(home)
+
 }
 
 function homeButton3() {
     console.log(`Home`, home += 3)
     let homeScore = document.getElementById(`homeScore`)
-    console.log(home)
     homeScore.innerText = home
-    console.log(home)
+
 }
 
 function awayButton1() {
     console.log(`Away`, away += 1)
     let awayScore = document.getElementById(`awayScore`)
     awayScore.innerText = away
-    console.log(awayScore)
+
 
 }
 
@@ -45,5 +39,5 @@ function awayButton3() {
     console.log(`Away`, away += 3)
     let awayScore = document.getElementById(`awayScore`)
     awayScore.innerText = away
-    console.log(awayScore)
+
 }
